242ae96f78d30b9f9c0ff9e8134bd91d89e77694
EG-Watanabe-Ryota <ryota.watanabe@e-grant.net>
2021-11-20 23:16:55 +0900

    コメント追加＆カート追加ボタンのvalueの値を変更

--- app/templates/Products/index.php
+++ app/templates/Products/index.php
@@ -42,16 +42,22 @@
                         <?php echo $this->Html->image(h($product->img),['width' => '237px','height' => '221px']); ?>
                         <p><?= h($product->name) ?></p>
                         <p><?= h($product->price) ?>円(税込)</p>
+
+                        <!-- 商品詳細ボタン-->
                         <button style="background-color:white; border-color:black; margin-top:10px;">
                             <?= $this->Html->link('商品詳細', ['action' => 'detail', $product->id]) ?>
                         </button>
-                        
+                        <!--フォーム送信-->
                         <form method="post" action="">
+
+                            <!--数量入力テキストボックス-->
                             <label style="display:block; margin:0 0;">
                                 数量:<input type="number" name="quantity"  min="0" style="width:62px; height:27px; background-color:white;" />
                             </label>
+
+                            <!--カート追加ボタン-->
                             <label style="display:block;">
-                                <button type='submit' name='action' value='send'>カートに追加</button>
+                                <button type='submit' name='product_id' value=<?=$product->id?>>カートに追加</button>
                             </label>
                         </form>
                     </div>
