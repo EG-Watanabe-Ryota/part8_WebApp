d647fa880010c905c267dfa31c1850048a434f32
EG-Watanabe-Ryota <ryota.watanabe@e-grant.net>
2021-11-22 21:18:08 +0900

    今日の作業分

---
+++ app/templates/Customers/index.php
@@ -0,0 +1,28 @@
+<?php
+    echo $this->Html->css('template');
+    echo $this->Html->css('products');
+?>
+
+<header>
+    <h1>Nabe</h1>
+    <div class='new_member'>
+
+    </div>
+
+    <div class='login'>
+
+    </div>
+
+    <div class='cart'>
+
+    </div>
+
+    <div class='header_bar'>
+        <ul>
+            <li>HOME</li>
+            <li>カテゴリ</li>
+            <li>カテゴリ</li> 
+            <li>カテゴリ</li>                                                    
+        </ul>
+    </div>
+</header>
